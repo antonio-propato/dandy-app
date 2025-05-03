40a79a3d171defc55a3426e6760e12bb027d5379
Antonio Propato <tonyprp@gmail.com>
2025-05-03 23:44:26 +0100

    styling added with log animation and messages when double tapped

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,10 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+    },
   },
   plugins: [],
 };
